7904e56cccfbfcba0c4f4924ec1dc316df528273
Benjamin Peters <benjamin.peters@gmail.com>
2018-01-08 18:47:52 -0500

    get socket and connect

--- server/server.js
+++ server/server.js
@@ -1,14 +1,26 @@
 const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io')
 const express = require('express');
 
 //local
 const publicPath = path.join(__dirname, '../public');
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
+//start express and http
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('New user connected');
+    socket.on('disconnect', () => {
+      console.log('User was disconnected');
+    })
+});
+
+server.listen(port, () => {
   console.log(`Server is up on ${port}`);
 });
